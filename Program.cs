aa7797c8de16aa9709b4816d7960e651b1030c39
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-09-18 07:28:17 -0500

    feat: :tada: incio de reporsotrio

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<int, string> dicccionario = new Dictionary<int, string>();
+        Console.WriteLine("El total de el elemento es: " + dicccionario.Count);
+        Dictionary<string, string> openWith = new Dictionary<string, string>();
+
+        openWith.Add("txt","notepad.exe");
+        openWith.Add("bmp","paint.exe");
+        openWith.Add("did","paint.exe");
+        openWith.Add("rft","wordpad.exe");
+        
+        Dictionary<string, string>.KeyCollection ketColl = openWith.Keys;
+
+        foreach (string key in ketColl){
+            Console.WriteLine(key);
+        }
+
+        // try
+        // {
+        //     openWith.Add("txt","winword.exe");
+        // }
+        // catch (ArgumentException)
+        // {
+        //     Console.WriteLine("un elemento con la llave \"txt\" esta presente");
+        // }
+    }
+
+
+}
